8099d195a2fee5a9aecfb1b31bc0c6d58346e9dd
Zygimantas <zygimantaskondratavicius@gmail.com>
2021-08-03 20:08:15 +0300

    finished

---
+++ multiples of 3 or 5/script.js
@@ -0,0 +1,15 @@
+// Atspausdinti skaiciu suma kurie dalinasi is 3 ar 5
+
+function numbers(sk) {
+    let suma = 0;
+    for (let i = 1; i < sk; i++){
+        if ( i % 3 === 0 || i % 5 === 0 ){
+            suma += i;
+        }
+    }
+    console.log(suma)
+}
+
+numbers(10);
+
+
