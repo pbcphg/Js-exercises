fc766e7a85655a8635c46fb6a2e63f8d55a6b6f8
Zygimantas <39376865+pbcphg@users.noreply.github.com>
2021-08-20 15:33:36 +0300

    Update script.js

--- multiples of 3 or 5/script.js
+++ multiples of 3 or 5/script.js
@@ -2,7 +2,7 @@
 
 function numbers(sk) {
     let suma = 0;
-    for (let i = 1; i < sk; i++){
+    for (let i = 1; i <= sk; i++){
         if ( i % 3 === 0 || i % 5 === 0 ){
             suma += i;
         }
